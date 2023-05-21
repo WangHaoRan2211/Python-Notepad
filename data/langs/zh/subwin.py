f75435cad858d1eda89a487577e094bebd43ec6a
WP122022 <112704328+WangHaoRan2211@users.noreply.github.com>
2023-05-21 13:39:53 +0800

    Add Language Files

---
+++ data/langs/zh/subwin.py
@@ -0,0 +1,28 @@
+#save as
+sas_wtitle='警告'
+sas_wtext='新文件路径将会把原文件路径替换'
+
+#themewin
+
+themewin_title='更改主题'
+themewin_light='浅色'
+themewin_dark='暗色'
+
+#fontwin
+
+fontwin_title='字体设置'
+
+
+#Long Texts
+
+uplog_title='1.2 release1更新'
+uplog_text='''更新:
+\t1.添加了英语支持，可通过更改"程序根目录\\data\\langs\\lang.txt"
+为“中文”或“English”来切换对应的语言
+\t2.优化了代码逻辑'''
+
+
+
+
+
+
