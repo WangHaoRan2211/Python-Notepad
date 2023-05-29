7c893387fb41781ba97ebdf741d351b131b55f8c
WP122022 <112704328+WangHaoRan2211@users.noreply.github.com>
2023-05-29 17:51:22 +0800

    Add files via upload

---
+++ data/langs/sp/lang_main.py
@@ -0,0 +1,62 @@
+filetype_lang=[('archivo de texto', '*.txt'), ('archivo python', '*.py *.pyw'), ('archivo de lenguaje c', '*.c *.cpp *.cs'),(' archivo CMD ',' *.cmd *.bat '), (' todos los archivos', '*.*')]
+nortitle='Cuaderno de notas -'
+title='Cuaderno de notas - sin título'
+new_window_title='Sin título'
+
+bing='Bing'
+baidu='Baidu'
+sougo='Sogou'
+_360='360'
+bb='Bilibili'
+bdtb='Baidu post Bar'
+
+
+apply='Aplicación'
+verinfo='Información de la versión'
+
+#menu action
+filemenu='Documentos'
+editmenu='Edición'
+setmenu='Configuración'
+winmenu='Ventana'
+semenu='Buscar'
+aboutmenu='Sobre'
+
+#file menu
+
+new='Nueva construcción'
+save='Guardar'
+saveas='Guardar como'
+open='Abrir'
+exit='Salida'
+
+#edit menu
+
+copy='Copiar'
+cut='Corte'
+paste='Pegar'
+selall='Selección completa'
+delete='Eliminar'
+tagset='Configuración de la etiqueta'
+undo='Revocar'
+redo='Recuperación'
+
+#setting menu
+
+fontset='Fuente'
+themeset='Tema'
+click_batch='Ejecutar archivo por lotes'
+run_py='Ejecutar Código Python'
+lang_set='Configuración del idioma'
+
+#window menu
+
+zoom='Maximizar'
+iconify='Minimizar'
+setico='Configurar el icono de la ventana principal'
+
+#about menu
+
+aboutnotepad='Sobre el cuaderno'
+uplog='Actualizar el registro'
+comhelp='Descripción de la orden'
