ba95fdd7661bba45133b871c4377d11012357b19
WP122022 <112704328+WangHaoRan2211@users.noreply.github.com>
2023-07-02 17:27:51 +0800

    Add files via upload

---
+++ data/langs/de/subwin.py
@@ -0,0 +1,44 @@
+#save as
+sas_wtitle='Warnung'
+sas_wtext='Der neue Dateipfad wird den alten Dateipfad ersetzen'
+
+#themewin
+
+themewin_title='Thema ändern'
+themewin_light='Hell'
+themewin_dark='Dunkel'
+
+#fontwin
+
+fontwin_title='Schriftart einstellen'
+
+#tagwin
+
+tagwin_title='Tag einstellen'
+tagwin_startext='Geben Sie die Startposition ein (Format: Zeile.Spalte, beginnend bei 1)'
+tagwin_endtext='Geben Sie die Endposition ein (Format: Zeile.Spalte, beginnend bei 1)'
+tagwin_bg='Geben Sie die Hintergrundfarbe ein'
+tagwin_fg='Geben Sie die Vordergrundfarbe ein'
+tagwin_name='Geben Sie den Tag-Namen ein'
+tagwin_add='Tag hinzufügen'
+tagwin_remove='Tag entfernen'
+
+#langwin
+
+langwin_title='Spracheinstellung'
+langwin_info='Die Spracheinstellung wird nach dem Neustart der Software wirksam'
+ok='OK'
+
+#Long Texts
+
+
+comhelp='''open, -O, --open, /open: Öffnet eine Datei automatisch  Verwendung: python main.py --open {filePath} [Encoding]
+filePath: Der Pfad der zu öffnenden Datei (kann ein relativer oder absoluter Pfad sein)  Encoding: Die Kodierung der zu öffnenden Datei (Standard UTF-8)
+
+/help, --help, -H, help: Hilfe-Fenster  Verwendung: python main.py --help
+
+/set-theme, --set-theme, -ST, set-theme: Startet das Notepad mit einem benutzerdefinierten Thema  Verwendung: python main.py --set-theme {ThemeName}
+ThemeName: Der Name des Themas (kann light oder dark sein)
+
+
+{ValueName}: Pflichtfeld              [ValueName]: Optional'''
