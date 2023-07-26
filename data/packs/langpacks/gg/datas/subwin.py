1102ab7c82f631d212b528c19b681714594953e1
WP122022 <112704328+WangHaoRan2211@users.noreply.github.com>
2023-07-26 12:35:33 +0800

    Add files via upload

---
+++ data/packs/langpacks/gg/datas/subwin.py
@@ -0,0 +1,52 @@
+#save as
+sas_wtitle='警告'
+sas_wtext='新文件路径将会把原文件路径替换'
+
+#themewin
+
+themewin_title='更改主题'
+themewin_light='浅色'
+themewin_dark='暗色'
+
+#fontwin
+
+fontwin_title='字体设置'
+
+#tagwin
+
+tagwin_title='Tag设置'
+tagwin_startext='输入开始位置(格式:行.列,从1开始)'
+tagwin_endtext='输入结束位置(格式:行.列,从1开始)'
+tagwin_bg='输入背景色'
+tagwin_fg='输入前景色'
+tagwin_name='输入Tag名'
+tagwin_add='添加Tag'
+tagwin_remove='移除Tag'
+
+#langwin
+
+langwin_title='语言设置'
+langwin_info='语言设置将在重启软件后生效'
+ok='确定'
+
+#Long Texts
+
+comhelp='''open、-O、--open、/open:自动打开文件  使用方法:python main.py --open {filePath} [Encoding]
+filePath:打开的文件路径(可以是相对路径或绝对路径)  Encoding:打开的文件使用编码(默认UTF-8)
+
+/help、--help、-H、help:说明窗口  使用方法:python main.py --help
+
+/set-theme、--set-theme、-ST、set-theme:以自定义的主题启动记事本  使用方法:python main.py --set-theme {ThemeName}
+ThemeName:主题名字(可选light或dark)
+
+
+{ValueName}:必填项              [ValueName]:选填项
+'''
+
+
+#find window
+find='查找'
+
+
+
+
